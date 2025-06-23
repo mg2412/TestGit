0a735c7b4b6e32606782c94dc534184493bd96af
mg2412 <40015931+mg2412@users.noreply.github.com>
2025-06-23 15:46:06 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
 @echo off
-echo <html><body><h1>Hello from Jenkins</h1></body></html> > output.html
+REM Create an HTML file
+(
+echo ^<html^>^<body^>^<h1^>Hello from Jenkins^</h1^>^</body^>^</html^>
+) > output.html
+
+REM Create a zip file
 powershell Compress-Archive -Path output.html -DestinationPath output.zip
